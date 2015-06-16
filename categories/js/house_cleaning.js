d648d2ff48efa1e3cded7af0253887d65e9b8938
Rakesh sharma <rakesh@parallellabs.io>
2015-06-16 11:50:41 +0530

    navigation added to category pages

--- categories/js/house_cleaning.js
+++ categories/js/house_cleaning.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
 
-	// custom dropdown script multiple
-	$('.dropdown-select').on( 'click', '.dropdown-menu li a', function() { 
-		var target = $(this).html();
-	   //Adds active class to selected item
-	   $(this).parents('.dropdown-menu').find('li').removeClass('active');
-	   $(this).parent('li').addClass('active');
-	   //Displays selected text on dropdown-toggle button
-	   $(this).parents('.dropdown-select').find('.dropdown-toggle').html(target + ' <span class="caret pull-right"><i class="fa fa-chevron-down"></i></span>');
-	});//end
+	
 });
